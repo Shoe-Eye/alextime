3c403af1a5e36c05305806c096fef377f5f3d878
Sergei Surovtsev <ssurovsev@gmail.com>
2023-12-29 12:45:38 +0300

    upd calls

--- scripts/transcribe-video-files.py
+++ scripts/transcribe-video-files.py
@@ -3,24 +3,34 @@ import json
 from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 from faster_whisper import WhisperModel
 
-model_size = "large-v2"
+model_size = "large-v3"
 
 # Run on GPU with FP16
 model = WhisperModel(model_size, device="cuda", compute_type="float16")
 
 files = os.listdir("./calls/")
-audio_files = [file for file in files if file.endswith(
-    ".mp3") or file.endswith(".m4a") or file.endswith(".ogg")]
+audio_files = [
+    file
+    for file in files
+    if file.endswith(".mp3") or file.endswith(".m4a") or file.endswith(".ogg")
+]
 
 
 for file in audio_files:
-
+    print("transcribing ", file)
     segments, _ = model.transcribe(
         "./calls/" + file,
         vad_filter=True,
         vad_parameters=dict(min_silence_duration_ms=500),
+        language="ru",
     )
     text = " ".join([segment.text for segment in segments])
     # save segments to json file
-    with open("transcripts/" + file + ".txt", "w") as f:
+    transcript_filename = (
+        ("transcripts/" + file + ".txt")
+        .replace(".mp3", "")
+        .replace(".m4a", "")
+        .replace(".ogg", "")
+    )
+    with open(transcript_filename, "w") as f:
         json.dump(text, f, indent=2, ensure_ascii=False)
